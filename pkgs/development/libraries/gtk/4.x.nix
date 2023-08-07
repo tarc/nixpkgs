b5a43f96bb34b103c733c392bb3677962e428d92
Bobby Rong <rjl931189261@126.com>
2023-08-07 07:57:29 +0000

    gtk4: 4.10.4 → 4.12.0
    https://gitlab.gnome.org/GNOME/gtk/-/compare/4.10.4...4.12.0

--- pkgs/development/libraries/gtk/4.x.nix
+++ pkgs/development/libraries/gtk/4.x.nix
@@ -64,7 +64,7 @@ in
 
 stdenv.mkDerivation rec {
   pname = "gtk4";
-  version = "4.10.4";
+  version = "4.12.0";
 
   outputs = [ "out" "dev" ] ++ lib.optionals x11Support [ "devdoc" ];
   outputBin = "dev";
@@ -76,7 +76,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gtk/${lib.versions.majorMinor version}/gtk-${version}.tar.xz";
-    sha256 = "dyVABILgaF4oJl4ibGKEf05zz8qem0FqxYOCB/U3eiQ=";
+    sha256 = "ptEIKfQFsa/Atl4qlkLAQSah0bY40Rxtl0JtpPhPH28=";
   };
 
   patches = [
